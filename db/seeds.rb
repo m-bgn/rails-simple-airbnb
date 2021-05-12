ac427b4d9795a859aac9975841a335ff7182cf97
Marc-Antoine Begin <m.begin@outlook.fr>
2021-05-12 17:13:23 +0200

    airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+5.times do
+  Flat.create(name: Faker::Artist.name,
+  address: Faker::Address.street_address,
+  description: Faker::Address.country,
+  price_per_night: Faker::Commerce.price(range: 0..1000).to_i, number_of_guests: Faker::Commerce.price(range: 0..20).to_i)
+end
